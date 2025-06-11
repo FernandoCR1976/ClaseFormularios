229f510b7b15ce64add23c1aa4692f0961fe0674
FernandoCR1976 <fernandocoralesm76@gmail.com>
2025-06-10 18:54:14 -0600

    se termina el script.js y se pasa a pruebas

--- script.js
+++ script.js
@@ -46,8 +46,45 @@ document.addEventListener('DOMContentLoaded',()=>{
             mostrarError(errorNombre,nombreInput,'El nombre es requerido');
             formValido = false;
         }
+        //Validacion para correo electronico
 
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (emailInput.value.trim() === ''){
+            mostrarError(errorEmail,emailInput,'El correo es requerido');
+            formValido = false;
+        }else if (!emailPattern.test(emailInput.value.trim())){
+            mostrarError(errorEmail,emailInput,'Ingrese un correo valido');
+            formValido = false;
+        }
+        //Validacion de la contraseña
+        if (passwordInput.value.trim() === ''){
+            mostrarError(errorPassword,passwordInput,'la contraseña es requerida');
+            formValido = false;
 
-    })
+        }else if (passwordInput.value.trim().length <6){
+            mostrarError(errorPassword,passwordInput,'La contreseña debe de ser mayor a 6');
+            formValido = false;
+        }
 
-})
+        //Validacion de la confirmacion de la contraseña
+
+        if (confirmPasswordInput.value.trim() === ''){
+            mostrarError(errorConfirmPassword,confirmPasswordInput,'Debe de reingresar la contraseña');
+            formValido = false;
+        }else if (confirmPasswordInput.value !== passwordInput.value){
+            mostrarError(errorConfirmPassword,confirmPasswordInput,'Las contraseñas deben ser iguales');
+            formValido = false;
+        }
+
+        //Si el formulario es valido, mostrar el mensaje de exito y resetear el formulario
+
+        if (formValido) {
+            successMessage.style.display = 'block';
+            registroForm.reset(); // Limpia todos los campos del formulario
+            console.log('Formulario enviado correctamente !');
+
+        }
+
+    });
+
+});
